b3df739e49eca27c670a4377f2ad9b3ee7eac223
ankitsinghmyself <ankitsinghmyself@gmail.com>
2020-06-21 12:01:01 +0530

    cookies fix

--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   //setting of default room
   let room = "Lounge";
   joinRoom("Lounge");
-  deleteAllCookies()
+
   //dispaly incomming msg
   socket.on('message', data => {
     const p = document.createElement('p');
@@ -123,14 +123,5 @@ document.addEventListener('DOMContentLoaded', () => {
     const chatWindow = document.querySelector("#display-message-section");
     chatWindow.scrollTop = chatWindow.scrollHeight;
 }
-function deleteAllCookies() {
-  var cookies = document.cookie.split(";");
 
-  for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i];
-      var eqPos = cookie.indexOf("=");
-      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-  }
-}
 });
